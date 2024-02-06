68d21c7a8335266e7fb23a004409f9e8dd4d20b6
rafaelolv <r.rafael.oliv@gmail.com>
2024-02-06 09:26:46 -0300

    primeiro commit opr

---
+++ frontend/src/pages/Teste.js
@@ -0,0 +1,18 @@
+import { React } from 'react';
+
+
+
+const Form = () => {
+
+
+    return (
+        <div className={styleFormRestaurante.boxForm}>
+
+            <p>Ajustes da reserva opr</p>
+            
+        </div>
+    )
+
+}
+
+export default Form;
